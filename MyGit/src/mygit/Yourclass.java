86f5de54a743779764c92499f99d318d4867c289
hsyan <hsyan@222.107.240.71>
2023-12-23 16:06:02 +0900

    Yourclass 추가

---
+++ MyGit/src/mygit/Yourclass.java
@@ -0,0 +1,11 @@
+package mygit;
+
+public class Yourclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git!");
+		System.out.println("Hello Git2!");
+	}
+
+}
